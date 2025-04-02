c9232caeb24c7c3a4046c79473407bfad2cf1fa2
Washington <washington195luiz@gmail.com>
2025-04-01 22:38:27 -0300

    First Commit

---
+++ src/main/java/br/com/washington/application_events/ApplicationEventsApplication.java
@@ -0,0 +1,13 @@
+package br.com.washington.application_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/washington/application_events/ApplicationEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.application_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
